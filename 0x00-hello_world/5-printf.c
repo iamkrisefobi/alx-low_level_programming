89c8ca5cedcaf4a4218d7ffba1ea9fcab025c38b
iamkrisefobi <krisfobbby@gmail.com>
2022-09-08 15:57:08 -0700

    This c program prints a string using the printf function

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+*main - prints a script
+*
+*Description: prints a script exactly as it is
+*
+*Return: 0
+*
+*/
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}

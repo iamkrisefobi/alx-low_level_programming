caefd054308562416bb2c79ee078d3e5eea917bd
iamkrisefobi <krisfobbby@gmail.com>
2022-09-08 15:42:32 -0700

    This c program prints a string

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+*main - prints a string that inputs the functions
+*Description: Printing "Programming is like building a multilingual puzzle"
+*Return: Always 0 (Success)
+*/
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
